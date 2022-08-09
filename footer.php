b644a3516c45c3a1cdc45e830b094e78fc118be1
“Haruna <ex436000@gmail.com>
2022-08-09 16:18:45 +0900

    jsファイルのパスを修正

---
+++ footer.php
@@ -0,0 +1,21 @@
+<!-- footer  -->
+<footer>
+    <ul>
+        <li><a href="about.html">私について</a></li>
+        <li><a href="works.html">制作実績</a></li>
+        <li><a href="#contact">お問い合わせ</a></li>
+    </ul>
+    <p class="copyright">&copy;Portfolio Haruna Tanaka</p>
+</footer>
+<!-- footer  -->
+
+<!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+<script src="js/slick.js"></script>
+<script src="js/main.js"></script> -->
+<script src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
+
+<?php wp_footer(); ?>
+</body>
+
+</html>
