d240eafc3cfca9a2772ac984a6b88ad3a809eb84
“Haruna <ex436000@gmail.com>
2022-08-13 14:32:49 +0900

    works.php - ヘッダー/フッター読み込み

--- works.php
+++ works.php
@@ -1,253 +1,164 @@
-<!DOCTYPE html>
-<html lang="ja">
+<?php get_header(); ?>
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Haruna-Portfolio</title>
-  <link rel="stylesheet"
-    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-  <!-- <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"> -->
-  <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
-  <link rel="stylesheet" type="text/css"
-    href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css" />
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Oooh+Baby&display=swap" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
-  <link rel="stylesheet" href="css/reset.css">
-  <link rel="stylesheet" href="scss/style.css">
-  <link rel="shortcut icon" href="img/favicon.ico">
-</head>
-
-<body>
-  <!-- header 始まり-->
-  <header>
-    <div class="nav-box wrapper">
-      <h1 class="logo"><a href="index.html"><img src="img/logo.png" alt="logo"></a></h1>
-      <div class="pc-nav">
-        <nav>
-          <ul class="nav-lists">
-            <li class="nav-list">
-              <a class="uLine" href="about.html">
-                <span class="uLine-center">私について</span>
-              </a>
-            </li>
-            <li class="nav-list">
-              <a class="uLine" href="works.html">
-                <span class="uLine-center">制作実績</span>
-              </a>
-            </li>
-            <li class="nav-list">
-              <a class="uLine" href="about.html">
-                <span class="uLine-center">お問い合わせ</span>
-              </a>
-            </li>
-          </ul>
-        </nav>
+<!-- main -->
+<!-- 実績一覧 -->
+<section class="worksPage">
+  <div class="wrapper">
+    <div class="works-flex">
+      <div class="section-title-center">
+        <h2 class="section-title">制作実績</h2>
       </div>
-      <!-- header 787px以下
-          ハンバーガーメニュー 開く-->
-      <div class="sp-icon">
-        <span class="material-icons" id="open">menu</span>
+      <div class="works-items">
+        <div class="works-item">
+          <div class="works-img">
+            <a href="works1.html" class="works_box">
+              <figure>
+                <img src="img/works1.png" alt="Webサイト1">
+                <figcaption class="hover-mask">詳細を見る</figcaption>
+              </figure>
+            </a>
+          </div>
+          <div class="works-text">
+            <dl>
+              <dt>カフェサイトの制作</dt>
+              <dd>制作課題として、模写コーディングしました。<br>
+                スクロール時、背景画像に動きをつける(レスポンシブ対応済)</dd>
+            </dl>
+          </div>
+        </div>
+        <div class="works-item">
+          <div class="works-img">
+            <a href="works2.html" class="works_box">
+              <figure>
+                <img src="img/works2.png" alt="Webサイト2">
+                <figcaption class="hover-mask">詳細を見る</figcaption>
+              </figure>
+            </a>
+          </div>
+          <div class="works-text">
+            <dl>
+              <dt>旅行ブログサイト</dt>
+              <dd>制作課題として、模写コーディングしました。<br>
+                スクロール固定したヘッダーを横にスクロールさせる(レスポンシブ対応済)</dd>
+            </dl>
+          </div>
+        </div>
+        <div class="works-item">
+          <div class="works-img">
+            <a href="works3.html" class="works_box">
+              <figure>
+                <img src="img/works3.png" alt="Webサイト3">
+                <figcaption class="hover-mask">詳細を見る</figcaption>
+              </figure>
+            </a>
+          </div>
+          <div class="works-text">
+            <dl>
+              <dt>ECサイト制作(インテリア)</dt>
+              <dd>制作課題として、模写コーディングしました。スクロールしてもヘッダーを上部に表示する、Googleマップの埋め込み</dd>
+            </dl>
+          </div>
+        </div>
+        <div class="works-item">
+          <div class="works-img">
+            <a href="works4.html" class="works_box">
+              <figure>
+                <img src="img/works4.png" alt="Webサイト4">
+                <figcaption class="hover-mask">詳細を見る</figcaption>
+              </figure>
+            </a>
+          </div>
+          <div class="works-text">
+            <dl>
+              <dt>スニーカーサイト</dt>
+              <dd>制作課題として、模写コーディングしました。<br>
+                動画の埋め込み、スライダー等を学び、実装しております。</dd>
+            </dl>
+          </div>
+        </div>
+        <div class="works-item">
+          <div class="works-img">
+            <a href="works5.html" class="works_box">
+              <figure>
+                <img src="img/works5.png" alt="Webサイト5">
+                <figcaption class="hover-mask">詳細を見る</figcaption>
+              </figure>
+            </a>
+          </div>
+          <div class="works-text">
+            <dl>
+              <dt>ポートフォリオ制作</dt>
+              <dd>コメントが入ります。コメントが入ります。コメントが入ります。</dd>
+            </dl>
+          </div>
+        </div>
       </div>
-  </header>
-
-  <!-- ハンバーガーメニュー 閉じる -->
-  <div class="overlay">
-    <span class="material-icons" id="close">close</span>
-    <!-- ハンバーガーメニュー 内容 -->
-    <nav class="sp-menu">
-      <ul class="sp-nav">
-        <li class="sp-nav-list">
-          <a class="uLine" href="about.html">
-            <span class="uLine-center">私について</span>
-          </a>
-        </li>
-        <li class="sp-nav-list">
-          <a class="uLine" href="works.html">
-            <span class="uLine-center">制作実績</span>
-          </a>
-        </li>
-        <li class="sp-nav-list">
-          <a class="uLine" href="#contact">
-            <span class="uLine-center">お問い合わせ</span>
-          </a>
-        </li>
-      </ul>
-    </nav>
+    </div>
   </div>
-  <!-- </div> -->
-  <!-- header 終わり-->
+</section>
+<!-- 実績一覧 -->
+<!-- main -->
 
-  <!-- main -->
-  <!-- 実績一覧 -->
-  <section class="worksPage">
-    <div class="wrapper">
-      <div class="works-flex">
-        <div class="section-title-center">
-          <h2 class="section-title">制作実績</h2>
-        </div>
-        <div class="works-items">
-            <div class="works-item">
-              <div class="works-img">
-                <a href="works1.html" class="works_box">
-                  <figure>
-                    <img src="img/works1.png" alt="Webサイト1">
-                    <figcaption class="hover-mask">詳細を見る</figcaption>
-                  </figure>
-                </a>
-              </div>
-              <div class="works-text">
-                <dl>
-                  <dt>カフェサイトの制作</dt>
-                  <dd>制作課題として、模写コーディングしました。<br>
-                    スクロール時、背景画像に動きをつける(レスポンシブ対応済)</dd>
-                </dl>
-              </div>
-            </div>
-            <div class="works-item">
-              <div class="works-img">
-                <a href="works2.html" class="works_box">
-                  <figure>
-                    <img src="img/works2.png" alt="Webサイト2">
-                    <figcaption class="hover-mask">詳細を見る</figcaption>
-                  </figure>
-                </a>
-              </div>
-              <div class="works-text">
-                <dl>
-                  <dt>旅行ブログサイト</dt>
-                  <dd>制作課題として、模写コーディングしました。<br>
-                    スクロール固定したヘッダーを横にスクロールさせる(レスポンシブ対応済)</dd>
-                </dl>
-              </div>
-            </div>
-            <div class="works-item">
-              <div class="works-img">
-                <a href="works3.html" class="works_box">
-                  <figure>
-                    <img src="img/works3.png" alt="Webサイト3">
-                    <figcaption class="hover-mask">詳細を見る</figcaption>
-                  </figure>
-                </a>
-              </div>
-              <div class="works-text">
-                <dl>
-                  <dt>ECサイト制作(インテリア)</dt>
-                  <dd>制作課題として、模写コーディングしました。スクロールしてもヘッダーを上部に表示する、Googleマップの埋め込み</dd>
-                </dl>
-              </div>
-            </div>
-            <div class="works-item">
-              <div class="works-img">
-                <a href="works4.html" class="works_box">
-                  <figure>
-                    <img src="img/works4.png" alt="Webサイト4">
-                    <figcaption class="hover-mask">詳細を見る</figcaption>
-                  </figure>
-                </a>
-              </div>
-              <div class="works-text">
-                <dl>
-                  <dt>スニーカーサイト</dt>
-                  <dd>制作課題として、模写コーディングしました。<br>
-                    動画の埋め込み、スライダー等を学び、実装しております。</dd>
-                </dl>
-              </div>
-            </div>
-            <div class="works-item">
-              <div class="works-img">
-                <a href="works5.html" class="works_box">
-                  <figure>
-                    <img src="img/works5.png" alt="Webサイト5">
-                    <figcaption class="hover-mask">詳細を見る</figcaption>
-                  </figure>
-                </a>
-              </div>
-              <div class="works-text">
-                <dl>
-                  <dt>ポートフォリオ制作</dt>
-                  <dd>コメントが入ります。コメントが入ります。コメントが入ります。</dd>
-                </dl>
-              </div>
-            </div>
-        </div>
-      </div>
-    </div>
-  </section>
-  <!-- 実績一覧 -->
-  <!-- main -->
-
-  <!--  section お問い合わせ-->
-  <section class="contact wrapper" id="contact">
-    <div class="section-title-center">
-      <h2 class="section-title">お問い合わせ</h2>
-    </div>
-    <div class="contact-contents">
-      <p class="contact-text">
-        ご質問などありましたら下記よりお気軽にお問い合わせください
-        <br><span>*</span>は入力必須項目となります
-      </p>
-      <form action="" method="post">
-        <div class="form-item">
-          <label for="name">名前<span>*</span></label>
-          <input type="text" id="name" name="name">
-        </div>
-        <div class="form-item">
-          <label for="company">会社名</label>
-          <input type="company" id="company" name="company">
-        </div>
-        <div class="form-item">
-          <label for="email">メールアドレス<span>*</span></label>
-          <input type="mail" id="email" name="email">
-        </div>
-        <div class="form-item">
-          <label for="category">カテゴリ</label>
-          <input type="category" id="category" name="category">
-          <select class="selectBox" name="selectBox">
-            <option value="">選択してください</option>
-            <option value="selectBox1">ご質問</option>
-            <option value="selectBox2">ご相談</option>
-            <option value="selectBox3">その他</option>
-          </select>
-        </div>
-        <div class="form-item">
-          <label for="message">お問い合わせ内容<span>*</span></label>
-          <textarea id="message" name="message" placeholder="お問い合わせ内容をご入力ください"></textarea>
-        </div>
-        <div class="form-btn">
-          <input type="submit" value="送信">
-        </div>
-      </form>
-    </div>
-  </section>
-  <!--  section お問い合わせ-->
-
-  <!-- footer  -->
-  <footer>
-    <ul>
-      <li><a href="about.html">私について</a></li>
-      <li><a href="works.html">制作実績</a></li>
-      <li><a href="#contact">お問い合わせ</a></li>
-    </ul>
-    <p class="copyright">&copy;Portfolio Haruna Tanaka</p>
-  </footer>
-  <!-- footer  -->
-  <!-- 上に戻るボタン -->
-  <div class="page-top">
-    <a href="#">
-      <span class="material-symbols-outlined">arrow_circle_up</span>
-    </a>
+<!--  section お問い合わせ-->
+<section class="contact wrapper" id="contact">
+  <div class="section-title-center">
+    <h2 class="section-title">お問い合わせ</h2>
   </div>
-  <!-- 上に戻るボタン -->
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"
-    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-  <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
-  <script src="js/slick.js"></script>
-  <script src="js/main.js"></script>
-</body>
+  <div class="contact-contents">
+    <p class="contact-text">
+      ご質問などありましたら下記よりお気軽にお問い合わせください
+      <br><span>*</span>は入力必須項目となります
+    </p>
+    <form action="" method="post">
+      <div class="form-item">
+        <label for="name">名前<span>*</span></label>
+        <input type="text" id="name" name="name">
+      </div>
+      <div class="form-item">
+        <label for="company">会社名</label>
+        <input type="company" id="company" name="company">
+      </div>
+      <div class="form-item">
+        <label for="email">メールアドレス<span>*</span></label>
+        <input type="mail" id="email" name="email">
+      </div>
+      <div class="form-item">
+        <label for="category">カテゴリ</label>
+        <input type="category" id="category" name="category">
+        <select class="selectBox" name="selectBox">
+          <option value="">選択してください</option>
+          <option value="selectBox1">ご質問</option>
+          <option value="selectBox2">ご相談</option>
+          <option value="selectBox3">その他</option>
+        </select>
+      </div>
+      <div class="form-item">
+        <label for="message">お問い合わせ内容<span>*</span></label>
+        <textarea id="message" name="message" placeholder="お問い合わせ内容をご入力ください"></textarea>
+      </div>
+      <div class="form-btn">
+        <input type="submit" value="送信">
+      </div>
+    </form>
+  </div>
+</section>
+<!--  section お問い合わせ-->
 
-</html>
+<!-- footer  -->
+<footer>
+  <ul>
+    <li><a href="about.html">私について</a></li>
+    <li><a href="works.html">制作実績</a></li>
+    <li><a href="#contact">お問い合わせ</a></li>
+  </ul>
+  <p class="copyright">&copy;Portfolio Haruna Tanaka</p>
+</footer>
+<!-- footer  -->
+<!-- 上に戻るボタン -->
+<div class="page-top">
+  <a href="#">
+    <span class="material-symbols-outlined">arrow_circle_up</span>
+  </a>
+</div>
+<!-- 上に戻るボタン -->
+<?php get_footer(); ?>
