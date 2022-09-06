f39e5270ed39655f701309620820f98f11325a74
“Haruna <ex436000@gmail.com>
2022-09-06 20:48:39 +0900

    コメントアウト修正

--- header.php
+++ header.php
@@ -7,14 +7,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- <title><?php echo bloginfo('name'); ?></title> -->
-    // Googleアイコン
+    <!-- Googleアイコン -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    // Googleフォント
+    <!-- Googleフォント -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Oooh+Baby&display=swap" rel="stylesheet">
-    // ファビコン
+    <!-- ファビコン -->
     <link rel="shortcut icon" href="<?php echo esc_url(get_theme_file_uri('img/Portfolio Favicon.png')); ?>">
     <?php wp_head(); ?>
 </head>
