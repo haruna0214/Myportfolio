79d5511741b56acab9c56f819f78fbd0a19e5cfb
“Haruna <ex436000@gmail.com>
2022-09-14 10:49:45 +0900

    お問い合わせ完了メールのファイル追加

---
+++ page-thanks.php
@@ -0,0 +1,16 @@
+/*
+Template Name: お問い合わせ(送信完了)
+*/
+<?php
+get_header();
+?>
+<main class="main">
+    <h1>送信完了のお知らせ</h1>
+    <p>
+        <!-- wp:paragraph -->
+    <p>Haruna | Portfolio へお問い合わせいただき誠にありがとうございます。<br>
+    ご入力いただきました情報を送信いたしましたのでお知らせいたします。</p>
+</main>
+<?php
+get_footer();
+?>
